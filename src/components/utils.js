07218d35cd354e6a44fae3a6517c8b1b6ce73618
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 06:35:19 +0000

    add format date helper function

--- src/components/utils.js
+++ src/components/utils.js
@@ -7,6 +7,61 @@ const helper = (() => {
     }
   }
 
+  const formatDate = (unix, offset, dateFormat = "full") => {
+    const date = fromUnixTime(unix + offset).toUTCString();
+    let dayOfWeek = date.slice(0, 3);
+    let dayOfMonth = date.slice(5, 7);
+    const month = date.slice(8, 11);
+    const year = date.slice(14, 16);
+    let suffix;
+
+    // change 01 to 1 etc
+    if (dayOfMonth < 10) {
+      dayOfMonth = dayOfMonth.slice(1);
+    }
+
+    // generate correct date suffix
+    if (dayOfMonth.slice(-1) === "1") {
+      suffix = "st";
+    } else if (dayOfMonth.slice(-1) === "2") {
+      suffix = "nd";
+    } else if (dayOfMonth.slice(-1) === "3") {
+      suffix = "rd";
+    } else {
+      suffix = "th";
+    }
+
+    // those pesky 11, 12, 13 ths
+    if (dayOfMonth > 3 && dayOfMonth < 21) {
+      suffix = "th";
+    }
+
+    // convert short day name to full day name
+    if (dayOfWeek === "Mon") {
+      dayOfWeek = "Monday";
+    } else if (dayOfWeek === "Tue") {
+      dayOfWeek = "Tuesday";
+    } else if (dayOfWeek === "Wed") {
+      dayOfWeek = "Wednesday";
+    } else if (dayOfWeek === "Thu") {
+      dayOfWeek = "Thursday";
+    } else if (dayOfWeek === "Fri") {
+      dayOfWeek = "Friday";
+    } else if (dayOfWeek === "Sat") {
+      dayOfWeek = "Saturday";
+    } else if (dayOfWeek === "Sun") {
+      dayOfWeek = "Sunday";
+    }
+
+    // return only the day of week
+    if (dateFormat === "day") {
+      return dayOfWeek;
+    }
+
+    // return full date string
+    return `${dayOfWeek}, ${dayOfMonth}${suffix} ${month} '${year}`;
+  };
+
   // convert utc string to format of 8:30 pm / 8:30 am
   // return just 9pm or 9am with timeFormat="hour"
   const formatTime = (unix, offset, timeFormat = "full") => {
@@ -52,6 +107,7 @@ const helper = (() => {
 
   return {
     removeAllChildNodes,
+    formatDate,
     formatTime,
     getIcon,
   };
