44721f8bc13525ad7d37a95dc4fd4fc13d7f69aa
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 06:18:08 +0000

    add helper function to format time from utc

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,5 @@
+import fromUnixTime from "date-fns/fromUnixTime/index.js";
+
 const helper = (() => {
   function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -5,8 +7,45 @@ const helper = (() => {
     }
   }
 
+  // convert utc string to format of 8:30 pm / 8:30 am
+  // return just 9pm or 9am with timeFormat="hour"
+  const formatTime = (unix, offset, timeFormat = "full") => {
+    const date = fromUnixTime(unix + offset).toUTCString();
+    let hour = date.slice(17, 19);
+    const minute = date.slice(20, 22);
+    let amOrPm;
+
+    if (hour > 11) {
+      amOrPm = "pm";
+    } else {
+      amOrPm = "am";
+    }
+
+    // change 24hr to 12hr time
+    if (hour > 12) {
+      hour -= 12;
+    }
+
+    // change am times to 12hr time
+    if (hour < 10 && amOrPm === "am") {
+      hour = hour.slice(1, 2);
+    }
+
+    // midnight formating
+    if (hour === "0") {
+      hour = 12;
+    }
+
+    // return just the hour
+    if (timeFormat === "hour") {
+      return `${hour} ${amOrPm}`;
+    }
+    return `${hour}:${minute} ${amOrPm}`;
+  };
+
   return {
     removeAllChildNodes,
+    formatTime,
   };
 })();
 
