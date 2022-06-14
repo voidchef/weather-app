da8988afb778803e2e991132c5ce4fd2e76383f2
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 06:20:30 +0000

    add and implement showing 24 hours weather

--- src/components/render.js
+++ src/components/render.js
@@ -1,6 +1,7 @@
 import { weather } from "./weather";
 import { search } from "./search";
 import { unitChange } from "./unitChange";
+import { helper } from "./utils.js";
 
 const render = (() => {
   let data;
@@ -21,9 +22,42 @@ const render = (() => {
     }° ${unitChange.getUnit()}`;
   };
 
+  const twentyFour = () => {
+    const container = document.querySelector(".twentyFour");
+    const offset = data.timezone_offset;
+
+    helper.removeAllChildNodes(container);
+
+    for (const hour in data.hourly) {
+      const unix_time = data.hourly[hour].dt;
+      const icon = data.hourly[hour].weather[0].icon;
+      const temp = data.hourly[hour].temp;
+
+      const block = document.createElement("div");
+      block.style.display = "grid";
+      block.style.gridTemplateRows = "1fr 3fr 1fr";
+      block.style.margin = "10px";
+
+      const timeBlock = document.createElement("div");
+      timeBlock.innerHTML = helper.formatTime(unix_time, offset, "hour");
+
+      const iconBlock = document.createElement("div");
+      iconBlock.innerHTML = `<img src=${helper.getIcon(icon)}>`;
+
+      const tempBlock = document.createElement("div");
+      tempBlock.innerHTML = temp;
+
+      block.appendChild(timeBlock);
+      block.appendChild(iconBlock);
+      block.appendChild(tempBlock);
+      container.appendChild(block);
+    }
+  };
+
   const renderPage = () => {
     data = weather.getData();
     current();
+    twentyFour()
   };
 
   return {
